b9730f43cb18ab8adfa6312a217a6d78265f0454
TZN <tzn@TZNs-Mac-mini.local>
2018-11-27 21:22:46 +0700

    Initial Commit

---
+++ Inventory Amanotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory Amanotes
+//
+//  Created by TZN on 27/11/2018.
+//  Copyright © 2018 TZN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Inventory AmanotesTests/Inventory_AmanotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Inventory_AmanotesTests.swift
+//  Inventory AmanotesTests
+//
+//  Created by TZN on 27/11/2018.
+//  Copyright © 2018 TZN. All rights reserved.
+//
+
+import XCTest
+@testable import Inventory_Amanotes
+
+class Inventory_AmanotesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
